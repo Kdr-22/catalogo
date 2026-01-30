973d5e10f19c916102162986551b91850074441f
kdrq-22 <keiberdrq22@gmail.com>
2026-01-29 23:16:24 -0400

    fix(backend): :bug: Import and configure CORS in app.js

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-
+const cors = require('cors')
 const path = require('path');
 const productRoutes = require('./routes/products');
 const morgan = require('morgan');
 
 const app = express();
 
-// Middlewares globales
-
+// Middlewares de seguridad y logs
+app.use(cors({
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type']
+}));
+// MIddlewares de parsing
 app.use(express.json());
 app.use(morgan('dev'));
 // Servir Front 
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas de la API
 app.use('/api/products', productRoutes);
