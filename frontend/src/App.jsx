b2b41fbab33eb83d4f795abbd6b49431da325b21
kdrq-22 <keiberdrq22@gmail.com>
2026-02-04 12:24:45 -0400

    test(frontend): :memo: improve fetching and useEffect structure to prevent auto-loop bug

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,17 @@ import './App.css'
 function App() {
   const [products, setProducts] = useState([])
 
-  async function getProducts() {
-    const res = await fetch('/api/products');
-    const datos = await res.json()
-    console.log(datos);
-    setProducts(datos)
-  }
   useEffect(()=>{
-    getProducts()
+    const getProducts = async () => {
+      const res = await fetch('/api/products');
+      const datos = await res.json()
+      console.log(datos);
+      setProducts(datos)
+    };
+    getProducts();
   },[]);
 
- 
+
   return (
     <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
       <h1>📦 Mi Catálogo</h1>
