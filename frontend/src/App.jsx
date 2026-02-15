d6ce2a698812202062eb5dcdea14f543c42ea4ce
Kdr-22 <keiberdrq22@gmail.com>
2026-02-15 13:50:30 -0400

    style(frontend): Define una nueva estructura para la UI basada en el diseño en figma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,114 +7,159 @@ import { DATOS } from "./services/mokup";
 // import { getProducts } from './services/api'
 
 function App() {
-  const [products, setProducts] = useState([])
-  const [productsPin, setProductsPin] = useState(()=>{
+  const [products, setProducts] = useState([]);
+  const [productsPin, setProductsPin] = useState(() => {
     const guardados = localStorage.getItem("mis-pines");
-    return guardados ? JSON.parse(guardados):[];
+    return guardados ? JSON.parse(guardados) : [];
   });
-  const [busqueda, setBusqueda] = useState("")
-
+  const [busqueda, setBusqueda] = useState("");
   const pinFunction = (nuevoProducto) => {
-
-    const yaExiste = productsPin.some(itemActual => itemActual.id === nuevoProducto.id)
-    if (yaExiste){
-      const nuevaLista = productsPin.filter(item => item.id !== nuevoProducto.id)
+    const yaExiste = productsPin.some(
+      (itemActual) => itemActual.id === nuevoProducto.id,
+    );
+    if (yaExiste) {
+      const nuevaLista = productsPin.filter(
+        (item) => item.id !== nuevoProducto.id,
+      );
       setProductsPin(nuevaLista);
       console.log("Eliminado de pines");
-
-    }else {
-      setProductsPin([...productsPin, nuevoProducto])
+    } else {
+      setProductsPin([...productsPin, nuevoProducto]);
       console.log("Agregado a pines");
     }
-    
-  }
-  const copyPaper = (producto)=> {
-    const textoACopiar = `📦 *${producto.valery_name}*\n` +
-                          `💰 Precio: $${producto.price}\n` +
-                          ` ℹ Detalle: ${producto.description || 'Consultar disponibilidad'}\n\n` +
-                          `📍 *Catálogo Digital*`;
+  };
+  const copyPaper = (producto) => {
+    const textoACopiar =
+      `📦 *${producto.valery_name}*\n` +
+      `💰 Precio: $${producto.price}\n` +
+      ` ℹ Detalle: ${producto.description || "Consultar disponibilidad"}\n\n` +
+      `📍 *Catálogo Digital*`;
 
-                          navigator.clipboard.writeText(textoACopiar).then(()=>{
-                            console.log("texto copiado al portapales")
-                          }).catch(err => {
-      console.error("Error al copiar: ", err);
-    });
-  }
-
-  useEffect(()=>{ 
+    navigator.clipboard
+      .writeText(textoACopiar)
+      .then(() => {
+        console.log("texto copiado al portapales");
+      })
+      .catch((err) => {
+        console.error("Error al copiar: ", err);
+      });
+  };
+  useEffect(() => {
     // Esta es una funcion auto ejecutable (Puedes tambier usar una funcion normal y llamarla)
     (async () => {
-      try{
+      try {
         // Las promesas deben ser esperadas "await" para que devuelvan un resultado que no sea "pending"
-        // const datos = await getProducts() 
-        setProducts(DATOS)        
-       
-      } catch(error){
+        // const datos = await getProducts()
+        setProducts(DATOS);
+      } catch (error) {
         console.error("Error al cargar los productos: ", error);
       }
-    })()
-  },[]);
-
-  useEffect(()=>{
-    localStorage.setItem('mis-pines', JSON.stringify(productsPin));
+    })();
+  }, []);
+  useEffect(() => {
+    localStorage.setItem("mis-pines", JSON.stringify(productsPin));
     console.log("LocalStorage actualizado con éxito");
-  }, [productsPin])
+  }, [productsPin]);
+  const productosFiltrados = products.filter((p) =>
+    p.valery_name.toLowerCase().includes(busqueda.toLowerCase()),
+  );
 
-  const productosFiltrados = products.filter((p) => 
-  p.valery_name.toLowerCase().includes(busqueda.toLowerCase())
-);
- return (
-    <main className='main'>
-        <header className='mainHeader'>
-
-          {/* <h1>Catalogo engine</h1> */}
-          <p className="mainDescription">Herramienta para facilidade de envio de productos y precios By KDR-22</p>
-          <div>
-
-          </div>
-        </header>
-         
-        <div className='mainContainer'>
-
-          <div className='productContainer'> 
-            <div className='productSearchDiv'>
-              <input 
-                className='productSearch'
-                type="text" 
-                placeholder="Buscar producto..." 
-                value={busqueda} 
-                onChange={(e) => setBusqueda(e.target.value)} 
-                />
-            </div>
-
-
-            <div className='cardContainer'>
-                {
-                  
-
-                  productosFiltrados.length === 0 ?
-                  <p>No hay elementos para cargar.</p> : (
-                  // Podemos pasar todos los props que queramos a un componente este los agrupara en un solo prop y podemos desestructurar para acceder a ellos
-                  productosFiltrados.map(p => {
-
-                    const isChecked = productsPin.some(item => item.id === p.id);
-
-                    return <ProductCard key={p.id} {...p} pinFunction={pinFunction} active={isChecked} copyPaper={copyPaper}/>
-                  })
-                  )
-                }
-            </div>
-          </div>
-            
-          <div className='productPin'>
-                {
-                  productsPin.map(p=> <ProductPIn key={p.id}{...p} pinFunction={pinFunction} copyPaper={copyPaper}/>)
-                }
+  const leftcontrols = (
+    <div className="controls PM-outline1pxsolidblack">
+      <div>
+        <div className="">
+          <button>Exportar grupo</button>
+          <button>Borrar todo</button>
+        </div>
+        <div className="">
+          {/* Insertar componente de 3 botones  */}
+          <button>Bs</button>
+          <button>$$</button>
+          <button>Bs-$$</button>
+        </div>
+      </div>
+      <div className="PM-outline1pxsolidblack PM-internalPadding">
+        <p>Colecciones</p>
+        <div>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+        </div>
+      </div>
+    </div>
+  );
+  const rightControls = (
+    <div className="rigth__controls PM-internalPadding PM-outline1pxsolidblack">
+      <div className="productSearchDiv  PM-outline1pxsolidblack">
+        <input
+          className="productSearch"
+          type="text"
+          placeholder="Buscar producto..."
+          value={busqueda}
+          onChange={(e) => setBusqueda(e.target.value)}
+        />
+      </div>
+      <div className="priceSelector PM-outline1pxsolidblack">
+        <p>Selector de $$ o Bs</p>
+      </div>
+    </div>
+  );
+  return (
+    <main className="main">
+      <div className="appContainer">
+        <div className="appContainer__left PM-internalPadding PM-outline1pxsolidblack">
+          {
+            /* controles para la gestion de elementos pineados  */
+            leftcontrols
+          }
+          <div className="pinElements PM-outline1pxsolidblack">
+            {productsPin.map((p) => (
+              <ProductPIn
+                key={p.id}
+                {...p}
+                pinFunction={pinFunction}
+                copyPaper={copyPaper}
+              />
+            ))}
           </div>
         </div>
 
+        <div className="appContainer__right">
+          <div className="rigth PM-outline1pxsolidblack">
+            {
+              /* Controles de busqueda de cartas y cambio en la muestra de precios */
+              rightControls
+            }
+
+            <div className="cardContainer PM-internalPadding PM-outline1pxsolidblack">
+              {productosFiltrados.length === 0 ? (
+                <p>No hay elementos para cargar.</p>
+              ) : (
+                // Podemos pasar todos los props que queramos a un componente este los agrupara en un solo prop y podemos desestructurar para acceder a ellos
+                productosFiltrados.map((p) => {
+                  const isChecked = productsPin.some(
+                    (item) => item.id === p.id,
+                  );
+
+                  return (
+                    <ProductCard
+                      key={p.id}
+                      {...p}
+                      pinFunction={pinFunction}
+                      active={isChecked}
+                      copyPaper={copyPaper}
+                    />
+                  );
+                })
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/ProductPIn/ProductPin.jsx
+++ frontend/src/components/ProductPIn/ProductPin.jsx
@@ -1,11 +1,13 @@
-import "./ProductPin.css"
-export function ProductPIn (props){
-    const {valery_name: name, price, pinFunction, copyPaper : copy} = props
-    return (
-        <div>
-          <p> {name} ${price}</p>
-          <button onClick={()=> pinFunction(props)}>🔻</button>
-            <button onClick={()=>copy(props)}>COpy</button>
-        </div>
-    )
+import "./ProductPin.css";
+export function ProductPIn(props) {
+  const { valery_name: name, price, pinFunction, copyPaper: copy } = props;
+  return (
+    <div>
+      <p>
+        {name} ${price}
+      </p>
+      <button onClick={() => pinFunction(props)}>X</button>
+      <button onClick={() => copy(props)}>COpy</button>
+    </div>
+  );
 }
