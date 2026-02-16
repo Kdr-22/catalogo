8dd18ac87b1da2b7cdab5548442b791c30100040
Kdr-22 <keiberdrq22@gmail.com>
2026-02-15 23:41:16 -0400

    style(frontend): Add Styles for Pinned cards elements

--- frontend/src/components/ProductPIn/ProductPin.jsx
+++ frontend/src/components/ProductPIn/ProductPin.jsx
@@ -1,15 +1,22 @@
 import "./ProductPin.css";
 import { CopyButtons } from "../CopyButtons/CopyButtons";
 export function ProductPIn(props) {
-  const { valery_name: name, price, pinFunction, copyPaper: copy } = props;
+  const {
+    valery_name: name,
+    price,
+    pinFunction,
+    //copyPaper: copy
+  } = props;
   return (
-    <div>
-      <p>
-        {name} ${price}
-      </p>
-      <button onClick={() => pinFunction(props)}>X</button>
-      <button onClick={() => copy(props)}>COpy</button>
-      <CopyButtons />
+    <div className="productPinned">
+      <div className="productPinned__top productPinned__inner">
+        <p>{name}</p>
+        <button onClick={() => pinFunction(props)}>X</button>
+      </div>
+      <div className="productPinned__bot productPinned__inner">
+        <p>$ {price}</p>
+        <CopyButtons />
+      </div>
     </div>
   );
 }
