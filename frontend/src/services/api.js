5b0e4586e7fa3d6c93d4e6e4a23763af26a5ebd8
Kdr-22 <keiberdrq22@gmail.com>
2026-02-27 23:03:46 -0400

    feat(frontend): Update Api conection url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,12 @@
-export const getProducts = async() => {
-    const url = "https://script.google.com/macros/s/AKfycbwOCRGTYg3RlZvfit_5uC3jPEld26TJJVhh4RoQ_2L3lUo3jAhCoOmP6lW5sTKhIIs/exec"
-    const res  = await fetch(url);
-    if (!res.ok) throw new Error("Falló al conectar")
-    return await res.json()
-}
+export const getProducts = async () => {
+  const url = "/api/products";
+  const res = await fetch(url);
+  if (!res.ok) throw new Error("Falló al conectar");
+  return await res.json();
+};
 
-export const delProducts = async(id)=>{
-    const res = await fetch(`/api/products/${id}`, {method: "DELETE"});
-    if (!res.ok) throw new Error ("Falló al borrar");
-    return res.ok
-}
+export const delProducts = async (id) => {
+  const res = await fetch(`/api/products/${id}`, { method: "DELETE" });
+  if (!res.ok) throw new Error("Falló al borrar");
+  return res.ok;
+};
